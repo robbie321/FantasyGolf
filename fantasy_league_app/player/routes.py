1fc6efbb33fa1801931c9eb651ee945c37ec812a
Rob Malone <rob.malone@digitalwell.com>
2025-08-27 12:18:49 +0100

    all players change

--- fantasy_league_app/player/routes.py
+++ fantasy_league_app/player/routes.py
@@ -104,20 +104,16 @@ def all_players():
 
     # Merge the two data sources
     combined_player_data = []
-    for player in local_players:
+    for player in sportradar_profiles:
         # Find the corresponding Sportradar profile by matching the full name
-        profile_data = sportradar_profiles.get(player.full_name())
-        
-        combined_player_data.append({
-            'player': player,
-            'profile': profile_data # This will be None if no match is found
-        })
+        # profile_data = sportradar_profiles.get(player.full_name())
+        combined_player_data.append(player)
 
 
-    # Sort the list of dictionaries by the 'last_name', then 'first_name'
-    sorted_players = sorted(combined_player_data, key=lambda p: (p.get('last_name', ''), p.get('first_name', '')))
+    # # Sort the list of dictionaries by the 'last_name', then 'first_name'
+    # sorted_players = sorted(combined_player_data, key=lambda p: (p.get('last_name', ''), p.get('first_name', '')))
 
-    return render_template('player/all_players.html', players=sportradar_profiles, title="All Players")
+    return render_template('player/all_players.html', players=combined_player_data, title="All Players")
 
 
 
